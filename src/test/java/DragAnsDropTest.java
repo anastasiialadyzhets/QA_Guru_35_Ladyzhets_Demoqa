236fc50ea6867fe4d4fe0ab564b56c63433d84bb
Anastasiia Ladyzhets <anastasiialadyzhets@Anastasiias-MacBook-Pro.local>
2025-04-21 01:45:05 +0300

    hw 5 hover & d&d

---
+++ src/test/java/DragAnsDropTest.java
@@ -0,0 +1,28 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAnsDropTest {
+    @BeforeAll
+    static void setup(){
+        Configuration.pageLoadStrategy= "eager";
+        Configuration.baseUrl="https://the-internet.herokuapp.com";
+        Configuration.browserSize="1920x1080";
+    }
+    @Test
+    void successDragAnsDropTest() {
+        open("/drag_and_drop");
+
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+
+        $("#column-a").dragAndDrop(DragAndDropOptions.to("#column-b"));
+
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
