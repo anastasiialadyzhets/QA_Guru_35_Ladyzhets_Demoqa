6d7dea6815980fe56710be907fbf3671245da720
Anastasiia Ladyzhets <anastasiialadyzhets@Anastasiias-MacBook-Pro.local>
2025-04-20 17:56:47 +0300

    hw 4 fix comments

--- src/test/java/SelenideGithubTest.java
+++ src/test/java/SelenideGithubTest.java
@@ -5,22 +5,54 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
 
 public class SelenideGithubTest {
     @BeforeAll
-    static void BeforeAll(){
+    static void beforeAll(){
         Configuration.pageLoadStrategy= "eager";
+        Configuration.baseUrl="https://github.com";
+        Configuration.browserSize="1920x1080";
     }
     @Test
     void successfulTest() {
-        open("https://github.com/selenide/selenide");
+        open("/selenide/selenide");
 
         $("[aria-label=Repository]").$(byText("Wiki")).click();
         $("[class='Box-row wiki-more-pages-link']").$("button").click();
         $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
         $("#wiki-pages-box").$(byText("SoftAssertions")).click();
 
-
         $("#wiki-body").$(byText("3. Using JUnit5 extend test class:")).shouldBe(visible);
+        String expectedCodeExample1= """
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }""";
+
+        String expectedCodeExample2= """
+                class Tests {
+                  @RegisterExtension\s
+                  static SoftAssertsExtension softAsserts = new SoftAssertsExtension();
+
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }""";
+
+        $("#wiki-body").$(byText("3. Using JUnit5 extend test class:")).closest(".markdown-heading").sibling(0).shouldHave(text(expectedCodeExample1));
+        $("#wiki-body").$(byText("3. Using JUnit5 extend test class:")).closest(".markdown-heading").sibling(2).shouldHave(text(expectedCodeExample2));
     }
 }
